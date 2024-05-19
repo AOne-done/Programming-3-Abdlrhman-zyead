e6f4b1c881b946ac0140bbc2997168e3a3da6e29
Abdlrhman zyead <141368903+AOne-done@users.noreply.github.com>
2024-05-19 20:58:42 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,13 +1,34 @@
 import socket
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(("localhost" , 9999))
+def run_client():
+    # create a socket object
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-done = False
-while not done :
-    client.send(input("Message : ").encode('utf-8'))
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'quit':
-        done =True
-    else :
-        print(msg)
-client.close()
+    server_ip = "127.0.0.1"  # replace with the server's IP address
+    server_port = 8000  # replace with the server's port number
+    # establish connection with server
+    client.connect((server_ip, server_port))
+
+    try:
+        while True: # in put while true because chating i want available 24h
+            # get input message from user and send it to the server
+            msg = input("Enter message: ")
+            client.send(msg.encode("utf-8")[:1024])
+
+            # receive message from the server
+            response = client.recv(1024)
+            response = response.decode("utf-8")
+
+            # if server sent us "closed" in the payload, we break out of
+            # the loop and close our socket
+            if response.lower() == "closed":
+                break
+            print(f"Received: {response}")
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # close client socket (connection to the server)
+        client.close()
+        print("Connection to server closed")
+
+run_client()
